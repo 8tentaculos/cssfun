2a11d6fb1edd5b9bed76ec35d069169b9250d2f5
Alberto Masuelli <alberto.masuelli@gmail.com>
2025-04-13 13:07:59 -0300

    Develop (#3)
    * Rename method
    
    * Improve attach and destroy
    
    * Add tests
    
    * Add shouldAddToDOM
    
    * Docs
    
    * Docs
    
    * Tests
    
    * Docs
    
    * Docs
    
    * Examples versions

--- src/StyleSheet.js
+++ src/StyleSheet.js
@@ -1,6 +1,8 @@
 const camelizedToDashed = str => str.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`);
 const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
 
+const styleSheetOptions = ['prefix', 'generateUid', 'generateClassName', 'shouldAddToDOM', 'attributes', 'renderers'];
+
 /**
  * The StyleSheet class receives at the constructor a styles object and an options
  * object and generate a css StyleSheet.  
@@ -28,6 +30,7 @@ const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
  * StyleSheet instance. These methods will be bound to the instance before they are invoked.
  * By default, `StyleSheet` are rendered using the built-in renderers: 
  * `['parseStyles', 'renderStyles']`.
+ * @param {Function} options.shouldAddToDOM - The function to determine if the StyleSheet should be added to the DOM.
  * @example
  * // Create a new StyleSheet instance with a styles object.
  * const instance = new StyleSheet({
@@ -58,7 +61,7 @@ class StyleSheet {
         // Original class names object.
         this.classes = {};
         // Set options on the instance.
-        ['prefix', 'generateUid', 'generateClassName', 'attributes', 'renderers'].forEach(key => {
+        styleSheetOptions.forEach(key => {
             if (key in options) this[key] = options[key];
         });
         // Set default renderers.
@@ -104,15 +107,6 @@ class StyleSheet {
      */
     generateClassName(className) {
         return `${this.uid}-${className}`;
-    }
-
-    /**
-     * Check if we are in the DOM.
-     * @returns {Boolean} True if we are in the DOM, false otherwise.
-     * @private
-     */
-    isDOM() {
-        return typeof document !== 'undefined';
     }
 
     /**
@@ -253,17 +247,29 @@ class StyleSheet {
     }
 
     /**
+     * Check if the StyleSheet should be added to the DOM.
+     * By default, it returns true if running in a browser environment and no style element
+     * with the same `data-fun-uid` attribute exists in the DOM.
+     * This prevents duplicate style elements and ensures proper behavior for server-side rendering.
+     * May be overridden by `options.shouldAddToDOM`.
+     * @returns {Boolean} True if the StyleSheet should be added to the DOM, false otherwise.
+     */
+    shouldAddToDOM() {
+        return typeof document !== 'undefined' && !document.querySelector(`style[data-${this.prefix}-uid="${this.uid}"]`);
+    }
+
+    /**
      * Add the instance to the registry and if we are in the browser, 
      * attach it to the DOM.
      * @returns {StyleSheet} The instance.
      */
     attach() {
         // Add the instance to the registry if it's not already there.
-        if (StyleSheet.registry.indexOf(this) === -1) {
+        if (!StyleSheet.registry.some(({ uid }) => uid === this.uid)) {
             StyleSheet.registry.push(this);
         }
         // If we're in the browser and the style element doesn't exist, create it.
-        if (this.isDOM() && !document.querySelector(`style[data-${this.prefix}-uid="${this.uid}"]`)) {
+        if (this.shouldAddToDOM()) {
             // Create the style element.
             this.el = document.createElement('style');
 
@@ -292,11 +298,15 @@ class StyleSheet {
         if (index > -1) {
             StyleSheet.registry.splice(index, 1);
         }
-        // Remove the style element from the DOM.
-        if (this.isDOM() && this.el) {
-            if (this.el.parentNode) this.el.parentNode.removeChild(this.el);
+
+        if (this.el) {
+            // Remove the style element from the DOM.
+            if (this.el.parentNode) {
+                this.el.parentNode.removeChild(this.el);
+            }
+            // Remove the reference to the style element.
             this.el = null;
-        }
+        } 
 
         return this;
     }
